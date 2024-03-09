7f548426b8d6c1bfcaa5ea0b71eea45aa2c04cd5
Tia Hedlund <tia.hedlund@elev.ga.ntig.se>
2024-03-09 11:32:43 +0100

    javascript för att spara i local påbörjad

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const kundvagnIkon = document.getElementById('cart');
-
 const kundvagnPil = document.getElementById('kundvagn-pil')
-
 const kundvagn = document.getElementById('kundvagn');
+let olKundvagn = document.querySelector("#olKundvagn");
 
+let arrayProduktKundvagn = [];
 
+// Öppna och stäng kundvagnen
 kundvagnIkon.addEventListener('click', function(){
     kundvagn.style.display = 'block';
 });
@@ -12,5 +13,66 @@ kundvagnIkon.addEventListener('click', function(){
 kundvagnPil.addEventListener('click', function() {
     kundvagn.style.display = 'none';
 });
+
+function add(namn, pris){
+    let produkt = {
+        pris: pris, 
+        namn: namn,
+
+    }
+    arrayProduktKundvagn.push(produkt);
+    console.log(arrayProduktKundvagn);
+}
+
+let jsonLi;
+let jsonVärde;
+
+/*skapa en array för artiklarna*/
+
+function addToOl (){
+    /*lägg till lista grej index*/ 
+    let liProdukt = document.createElement("li");
+    liProdukt.textContent = namn+pris;
+
+    olKundvagn.appendChild(liProdukt);
+
+    jsonLi = JSON.stringify(arrayProduktKundvagn);
+    window.localStorage.setItem(521, jsonLi);
+}
+
+function hämtaFrånLocal(){
+    jsonVärde = window.localStorage.getItem(521);
+    if (jsonVärde){
+        arrayProduktKundvagn = JSON.parse(jsonVärde);
+        addToOl();
+    }
+}
+
+hämtaFrånLocal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createCartItem(name, price, imgSrc) {
+    return `
+    <div class="bild">
+        <img src="${imgSrc}" alt="">
+    </div>
+    <div class="vara-text">
+        <div class="name">${name}</div>
+        <div class="pris">${price}</div>
+    </div>
+    <button>TA BORT</button>
+    `;
+}
 
 
